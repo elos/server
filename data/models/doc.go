3fca3c8b83205c3de91256179466b80792dcdd46
Nick Landolfi <nclandolfi@gmail.com>
2015-01-23 12:31:43 -0800

    add models/doc.go file

---
+++ data/models/doc.go
@@ -0,0 +1,8 @@
+/*
+
+The Models Package supplies the domain specific data interfaces elos relies on.
+
+These models are forced to fulfill the data.Record interface, so that they can be Persisted.
+
+*/
+package models
