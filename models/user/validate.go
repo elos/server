17f555273153ee6c4fce46eb968c4f0b3aabcdf5
Nick Landolfi <nclandolfi@gmail.com>
2015-01-30 23:52:13 -0800

    remove err globals in favor of data.AttrErrors

--- models/user/validate.go
+++ models/user/validate.go
@@ -1,20 +1,17 @@
 package user
 
 import (
-	"errors"
+	"github.com/elos/data"
 	"github.com/elos/server/models"
 )
 
-var NoNameError = errors.New("Error: user must have a name")
-var NoKeyError = errors.New("Error: user must have a key")
-
 func Validate(u models.User) (bool, error) {
 	if u.Name() == "" {
-		return false, NoNameError
+		return false, data.NewAttrError("name", "be present")
 	}
 
 	if u.Key() == "" {
-		return false, NoKeyError
+		return false, data.NewAttrError("key", "be present")
 	}
 
 	return true, nil
