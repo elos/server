67f685c41cfa9453c723ef7f4973aaf3b5da0bc0
Nick Landolfi <nclandolfi@gmail.com>
2016-01-13 15:30:26 -0800

    get server building

--- server.go
+++ server.go
@@ -6,17 +6,17 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
-	"github.com/elos/app"
-	"github.com/elos/app/middleware"
-	"github.com/elos/app/routes"
+	"github.com/elos/api"
+	apimiddleware "github.com/elos/api/middleware"
+	apiroutes "github.com/elos/api/routes"
 	"github.com/elos/autonomous"
+	"github.com/elos/data"
 	"github.com/elos/data/builtin/mongo"
-	"github.com/elos/ehttp/builtin"
 	emiddleware "github.com/elos/ehttp/middleware"
 	"github.com/elos/ehttp/serve"
 	"github.com/elos/models"
-	"github.com/gorilla/context"
 )
 
 var (
@@ -48,33 +48,103 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//api := api.New(db, hub)
-	//apiServer := serve.New(&serve.Opts{Handler: api})
+	Sandbox(db)
 
-	sessions := builtin.NewSessions()
-	app := app.New(&app.Middleware{
-		Log:         emiddleware.LogRequest,
-		SessionAuth: middleware.NewSessionAuth(db, sessions, routes.SessionsSignIn),
-	}, &app.Services{
-		Agents:   hub,
-		DB:       db,
-		Sessions: sessions,
+	api := api.New(&api.Middleware{
+		Cors: new(apimiddleware.Cors),
+		Log:  emiddleware.LogRequest,
+		SessionAuth: &apimiddleware.SessionAuth{
+			DB:                  db,
+			UnauthorizedHandler: apiroutes.Unauthorized,
+		},
+	}, &api.Services{
+		DB:     db,
+		Agents: hub,
 	})
+	apiServer := serve.New(
+		&serve.Opts{
+			Handler: api,
+		},
+	)
 
-	appServer := serve.New(&serve.Opts{
-		Handler: context.ClearHandler(app),
-		Port:    8080,
-	})
+	/*
+		sessions := builtin.NewSessions()
+		app := app.New(&app.Middleware{
+			Log:         emiddleware.LogRequest,
+			SessionAuth: middleware.NewSessionAuth(db, sessions, routes.SessionsSignIn),
+		}, &app.Services{
+			Agents:   hub,
+			DB:       db,
+			Sessions: sessions,
+		})
 
-	hub.StartAgent(appServer)
+		appServer := serve.New(&serve.Opts{
+			Handler: context.ClearHandler(app),
+			Port:    8080,
+		})
+	*/
 
-	u := &models.User{}
-	u.SetID(db.NewID())
-	u.Key = "password"
-	if err := db.Save(u); err != nil {
-		log.Fatal(err)
-	}
+	//hub.StartAgent(appServer)
+	hub.StartAgent(apiServer)
 
 	go autonomous.HandleIntercept(hub.Stop)
 	hub.WaitStop()
 }
+
+// --- Sandbox {{{
+func Sandbox(db data.DB) {
+	u := models.NewUser()
+	u.SetID(db.NewID())
+
+	c := models.NewCredential()
+	c.SetID(db.NewID())
+	c.Public = "public"
+	c.Private = "private"
+
+	c.SetOwner(u)
+	u.IncludeCredential(c)
+
+	if err := db.Save(u); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Save(c); err != nil {
+		log.Fatal(err)
+	}
+
+	p := models.NewPerson()
+	p.SetID(db.NewID())
+	if err := db.Save(p); err != nil {
+		log.Fatal(err)
+	}
+
+	cal, err := p.CalendarOrCreate(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := cal.BaseScheduleOrCreate(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f := models.NewFixture()
+	f.SetID(db.NewID())
+	f.Name = "Test Base Fixture"
+	f.StartTime = time.Now()
+	f.EndTime = time.Now().Add(1 * time.Hour)
+	if err := db.Save(f); err != nil {
+		log.Fatal(err)
+	}
+	b.IncludeFixture(f)
+
+	if err := db.Save(b); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Save(cal); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// --- }}}
